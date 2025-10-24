da3617afe4a78da605d208985f73dff169aa6b30
ManuBruzzone <manubruzzone2@gmail.com>
2025-10-24 15:11:14 -0300

    Componentes client

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        
+        path: '',
+        redirectTo: 'publicaciones',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login').then((archivo) => archivo.Login),
+    },
+    {
+        path: 'registro',
+        loadComponent: () =>
+            import('./pages/registro/registro').then((archivo) => archivo.Registro),
+    },
+    {
+        path: 'publicaciones',
+        loadComponent: () =>
+            import('./pages/publicaciones/publicaciones').then((archivo) => archivo.Publicaciones),
+    },
+    {
+        path: 'miperfil',
+        loadComponent: () =>
+            import('./pages/miperfil/miperfil').then((archivo) => archivo.Miperfil),
+    },
+    {
+        path: '**',
+        loadComponent: () =>
+            import('./pages/error/error').then((archivo) => archivo.Error),
+    }
+];
